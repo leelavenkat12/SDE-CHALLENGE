d674bcb28713378844c68ef05587bcfdd9d3946c
leela venkat <126691297+leelavenkat12@users.noreply.github.com>
2026-06-03 19:55:10 +0530

    Add merge function for sorted arrays
    Implement merge function to combine two sorted arrays.

---
+++ Merge Sorted Array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int pt1=m-1;
+        int pt2=n-1;
+        int pt3=m+n-1;
+        while(pt1>=0&&pt2>=0&&pt3>=0){
+            if(nums1[pt1]>nums2[pt2]){
+                nums1[pt3]=nums1[pt1];
+                pt1--;
+            }
+            else if(nums2[pt2]>=nums1[pt1]){
+                nums1[pt3]=nums2[pt2];
+                pt2--;
+            }
+
+            pt3--;
+        }
+        while(pt2>=0){
+            nums1[pt3]=nums2[pt2];
+            pt2--;
+            pt3--;
+        }
+
+    }
+};
