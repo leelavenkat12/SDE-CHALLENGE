cfb6037276ff8f3470e59068d207e60b6e97a055
leela venkat <126691297+leelavenkat12@users.noreply.github.com>
2026-06-09 20:59:32 +0530

    Implement majority element finder using map

---
+++ majorityelemnt1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        map<int,int>freq;
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
+        }
+        for(auto it:freq){
+            if(it.second >n/2){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
